61ded501874db480f14625c0c7ec31ffbb475453
Ahmad Fawzy <ahmedfcihfawzy@gmail.com>
2024-07-28 12:30:45 +0200

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    post 'login', to: 'users/sessions#create'
+    delete 'logout', to: 'users/sessions#destroy'
+    post 'signup', to: 'users/registrations#create'
+  end
+
 end
